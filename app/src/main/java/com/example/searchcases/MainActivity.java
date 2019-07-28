ec91a1dfb75567b58c59110838bc183179d3fe22
Antis <antis28@mail.ru>
2019-07-28 19:32:54 +0300

    add настройка макета и поведения
    java edit
    xml edit

--- app/src/main/java/com/example/searchcases/MainActivity.java
+++ app/src/main/java/com/example/searchcases/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -11,13 +13,14 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<String> list = new ArrayList<String>();
+
 /*
         try {
             list = extractFromXML();
@@ -30,7 +33,28 @@ public class MainActivity extends ListActivity {
         setListAdapter(new ArrayAdapter<String>(
                 this, android.R.layout.simple_list_item_1, list));
         */
-        parseRegistry();
+
+/*
+        ListView listView = (ListView) findViewById(R.id.listView);
+        List<PensionInfo> personList = parseRegistry();
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < personList.size(); i++) {
+            String fartherName = personList.get(i).getFartherName();
+            String name = personList.get(i).getName();
+            String lastName = personList.get(i).getLastName();
+            String numberRegistry = personList.get(i).getNumberRegistry();
+            String numberInBase = personList.get(i).getNumberInBase();
+
+            list.add(lastName + " " + name + " " + fartherName + " - "
+                    +  numberInBase + " **** " + numberRegistry);
+        }
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(adapter);
+        */
     }
 
     public ArrayList<String> extractFromXML() throws XmlPullParserException, IOException {
@@ -52,124 +76,36 @@ public class MainActivity extends ListActivity {
         return list;
     }
 
-    public void parseRegistry() {
-        PensionInfo person = null;
-        String registryName = "";
+    public List<PensionInfo> parseRegistry() {
+        List<PensionInfo> personList = null;
         try {
             XmlPullParser parser = getResources().getXml(R.xml.export_2019);
-            person = fillPensioner(parser);
+            personList = fillOutPensionerList(parser);
         } catch (Throwable t) {
             Toast.makeText(this,
                     "Ошибка при загрузке XML-документа: " + t.toString(),
                     Toast.LENGTH_LONG).show();
         }
+        String ggg = "GGG";
+        return personList;
     }
 
-    private String parseRegistryName(XmlPullParser parser) {
-        if (parser.getAttributeCount() == 0) {
-            return null;
-        }
-        String attributeName = parser.getAttributeName(0);
-        String attributeValue = parser.getAttributeValue(0);
-        String attributeName2 = parser.getAttributeValue(null, "name");
-        return attributeValue;
-    }
-
-    private PensionInfo parsePerson(XmlPullParser parser, PensionInfo personOrig) {
-        String nameTag = parser.getName();
-        PensionInfo person = new PensionInfo(personOrig);
-
-        final String NUMBER_IN_BASE = "numberInBase";
-        final String NUMBER_REGISTR = "numberRegistr";
-        final String DATA_START = "dataStart";
-        final String DATA_END = "dataEnd";
-        final String PAGE_COUNT = "pageCount";
-        final String REMARK = "remark";
-
-        parser.getAttributeValue(null,NUMBER_IN_BASE);
-        parser.getAttributeValue(null,NUMBER_REGISTR);
-        parser.getAttributeValue(null,DATA_START);
-        parser.getAttributeValue(null,DATA_END);
-        parser.getAttributeValue(null,PAGE_COUNT);
-        parser.getAttributeValue(null,REMARK);
-
-
-        for (int i = 0; i < parser.getAttributeCount(); i++) {
-            String attributeName = parser.getAttributeName(i);
-            String attributeValue = parser.getAttributeValue(i);
-
-            switch (attributeName) {
-                case "numberInBase":
-                    person.setNumberInBase(attributeValue);
-                    break;
-                case "numberRegistry":
-                    person.setNumberRegistry(attributeValue);
-                    break;
-                case "dataStart":
-                    person.setDataStart(attributeValue);
-                    break;
-                case "dataEnd":
-                    person.setDataEnd(attributeValue);
-                    break;
-                case "pageCount":
-                    person.setPageCount(attributeValue);
-                    break;
-                case "remark":
-                    person.setRemark(attributeValue);
-                    break;
-
-            }
-        }
-        return person;
-    }
-
-    private PensionInfo parseFullName(XmlPullParser parser, PensionInfo personOrig) {
-        PensionInfo person = new PensionInfo(personOrig);
-        for (int i = 0; i < parser.getAttributeCount(); i++) {
-            String attributeName = parser.getAttributeName(i);
-            String attributeValue = parser.getAttributeValue(i);
-
-            switch (attributeName) {
-                case "lastName":
-                    person.setLastName(attributeValue);
-                    break;
-                case "name":
-                    person.setName(attributeValue);
-                    break;
-                case "fartherName":
-                    person.setFartherName(attributeValue);
-                    break;
-                default:
-                    person.setRegistryName("2019");
-                    break;
-            }
-        }
-        return person;
-    }
-
-    private PensionInfo fillPensioner(XmlPullParser parser) throws XmlPullParserException, IOException {
+    private List<PensionInfo> fillOutPensionerList(XmlPullParser parser) throws XmlPullParserException, IOException {
         PensionInfo person = new PensionInfo();
+        List<PensionInfo> personList = new ArrayList<>();
+        String registryName = "";
 
         final String PERSON_TAG = "person";
         final String FULL_NAME_TAG = "fullName";
         final String REGISTER_TAG = "Register";
-
-        final String LAST_NAME = "lastName";
-        final String NAME = "name";
-        final String FARTHER_NAME = "fartherName";
-
 
         while (parser.getEventType() != XmlPullParser.END_DOCUMENT) {
 
             int eventType = parser.getEventType();
             // начало тэга
             if (eventType == XmlPullParser.START_TAG) {
-                //имя тега
-                // parser.getName();
-                //уровень тега
-                //parser.getDepth();
                 String nameTag = parser.getName();
-                switch (nameTag){
+                switch (nameTag) {
                     case PERSON_TAG:
                         person = parsePerson(parser, person);
                         break;
@@ -181,18 +117,91 @@ public class MainActivity extends ListActivity {
                         break;
                 }
             }
-            if (eventType == XmlPullParser.END_TAG && parser.getName() == PERSON_TAG)
-            {
+            if (eventType == XmlPullParser.END_TAG && parser.getName() == PERSON_TAG) {
                 if (person != null)
                     person.setRegistryName(registryName);
-                //languages.add(lang);
+                personList.add(person);
             }
             parser.next();
         }
 
 
+        return personList;
+    }
+
+    private String parseRegistryName(XmlPullParser parser) {
+        if (parser.getAttributeCount() == 0) {
+            return null;
+        }
+        String attributeValue = parser.getAttributeValue(0);
+        return attributeValue;
+    }
+
+    private PensionInfo parsePerson(XmlPullParser parser, PensionInfo personOrig) {
+        PensionInfo person = new PensionInfo(personOrig);
+
+        final String NUMBER_IN_BASE = "numberInBase";
+        final String NUMBER_REGISTR = "numberRegistr";
+        final String DATA_START = "dataStart";
+        final String DATA_END = "dataEnd";
+        final String PAGE_COUNT = "pageCount";
+        final String REMARK = "remark";
+
+
+        for (int i = 0; i < parser.getAttributeCount(); i++) {
+            String attributeName = parser.getAttributeName(i);
+            String attributeValue = parser.getAttributeValue(i);
+
+            switch (attributeName) {
+                case NUMBER_IN_BASE:
+                    person.setNumberInBase(attributeValue);
+                    break;
+                case NUMBER_REGISTR:
+                    person.setNumberRegistry(attributeValue);
+                    break;
+                case DATA_START:
+                    person.setDataStart(attributeValue);
+                    break;
+                case DATA_END:
+                    person.setDataEnd(attributeValue);
+                    break;
+                case PAGE_COUNT:
+                    person.setPageCount(attributeValue);
+                    break;
+                case REMARK:
+                    person.setRemark(attributeValue);
+                    break;
+            }
+        }
         return person;
     }
+
+    private PensionInfo parseFullName(XmlPullParser parser, PensionInfo personOrig) {
+        final String LAST_NAME = "lastName";
+        final String NAME = "name";
+        final String FARTHER_NAME = "fartherName";
+
+        PensionInfo person = new PensionInfo(personOrig);
+
+        for (int i = 0; i < parser.getAttributeCount(); i++) {
+            String attributeName = parser.getAttributeName(i);
+            String attributeValue = parser.getAttributeValue(i);
+
+            switch (attributeName) {
+                case LAST_NAME:
+                    person.setLastName(attributeValue);
+                    break;
+                case NAME:
+                    person.setName(attributeValue);
+                    break;
+                case FARTHER_NAME:
+                    person.setFartherName(attributeValue);
+                    break;
+            }
+        }
+        return person;
+    }
+
 
     public void someM() {
         try {

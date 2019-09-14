bb285928c353d4e303f2aa8db0de54052c3142ce
Antis <antis28@mail.ru>
2019-09-14 20:41:37 +0300

    add добработка нажатий при поиске по номеру в описи

--- app/src/main/java/com/example/searchcases/SearchByNumberRegistryActivity.java
+++ app/src/main/java/com/example/searchcases/SearchByNumberRegistryActivity.java
@@ -2,38 +2,54 @@ package com.example.searchcases;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.searchcases.MyUtils.KeyboardUtils;
 
 import java.util.List;
 
-public class SearchByNumberRegistryActivity extends AppCompatActivity {
+public class SearchByNumberRegistryActivity extends AppCompatActivity implements View.OnClickListener,
+        View.OnKeyListener{
+
     SearchManager searchManager;
+
+    Button btnSearchByNumberInRegistry,
+            btnClearTextEdit,
+            btnBack;
+    EditText etNumberInRegistry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_by_number_registry);
+
+        initListener();
+        KeyboardUtils.initFocusAndShowKeyBoard(etNumberInRegistry,this);
     }
 
-    public void buttonsHandler(View view) {
-        switch (view.getId()) {
-            case R.id.search_by_number_in_registry:
-                searchComparePensioner(view);
-                break;
-            case R.id.clear_button:
-                clearInputNumberInBase(view);
-                break;
-            case R.id.back_button:
-                onBackPressed();
-                break;
-        }
+    private void initListener() {
+        // Добавляем слушателя к компонентам
+        btnSearchByNumberInRegistry = findViewById(R.id.search_by_number_in_registry);
+        btnSearchByNumberInRegistry.setOnClickListener(this);
+
+        btnClearTextEdit = findViewById(R.id.clear_button);
+        btnClearTextEdit.setOnClickListener(this);
+
+        btnBack = findViewById(R.id.back_button);
+        btnBack.setOnClickListener(this);
+
+        etNumberInRegistry = findViewById(R.id.input_number_in_registry);
+        etNumberInRegistry.setOnKeyListener(this);
     }
 
-    private void searchComparePensioner(View view) {
+    private void searchComparePensioner() {
         EditText numberInRegistryTextView = findViewById(R.id.input_number_in_registry);
         String numberRegistry = numberInRegistryTextView.getText().toString();
 
@@ -77,5 +93,33 @@ public class SearchByNumberRegistryActivity extends AppCompatActivity {
     private void clearInputNumberInBase(View view) {
         EditText numberTextView = findViewById(R.id.input_number_in_registry);
         numberTextView.setText("");
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.search_by_number_in_registry:
+                searchComparePensioner();
+                KeyboardUtils.hideKeyBoard(etNumberInRegistry, this);
+                break;
+            case R.id.clear_button:
+                clearInputNumberInBase(view);
+                KeyboardUtils.initFocusAndShowKeyBoard(etNumberInRegistry,this);
+                break;
+            case R.id.back_button:
+                onBackPressed();
+                break;
+        }
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        // Обработайте нажатие клавиши, верните true, если
+        // обработка выполнена
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            searchComparePensioner();
+            KeyboardUtils.hideKeyBoard(etNumberInRegistry, this);
+        }
+        return false;
     }
 }

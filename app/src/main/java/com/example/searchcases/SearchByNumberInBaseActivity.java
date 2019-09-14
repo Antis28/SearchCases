b6c73cc36e4e7b0e5a795e689b475df2674e256f
Antis <antis28@mail.ru>
2019-09-14 20:25:11 +0300

    add работа с фокусом в активности
    - при запуске активности появляется клавиатура и передается фокус EditText;
    - при нажатии кнопки очистка также вызывается клавиатура и фокус;
    - принажатии ENTER на клавиатуре вызывается поиск и скрытие клавиатуры.

--- app/src/main/java/com/example/searchcases/SearchByNumberInBaseActivity.java
+++ app/src/main/java/com/example/searchcases/SearchByNumberInBaseActivity.java
@@ -2,26 +2,39 @@ package com.example.searchcases;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.example.searchcases.MyUtils.KeyboardUtils;
 
 import java.util.List;
 
-public class SearchByNumberInBaseActivity extends AppCompatActivity implements View.OnClickListener {
+public class SearchByNumberInBaseActivity extends AppCompatActivity implements View.OnClickListener,
+        View.OnKeyListener {
     SearchManager searchManager;
     Button btnSearchByNumberInBase,
             btnClearTextEdit,
             btnBack;
+    EditText etNumberInBase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_by_number_in_base);
 
+        initListener();
+        KeyboardUtils.initFocusAndShowKeyBoard(etNumberInBase, this);
+    }
+
+    private void initListener() {
+        // Добавляем слушателя к компонентам
         btnSearchByNumberInBase = findViewById(R.id.search_by_number_in_base);
         btnSearchByNumberInBase.setOnClickListener(this);
 
@@ -30,6 +43,9 @@ public class SearchByNumberInBaseActivity extends AppCompatActivity implements V
 
         btnBack = findViewById(R.id.back_button);
         btnBack.setOnClickListener(this);
+
+        etNumberInBase = findViewById(R.id.input_number_in_base);
+        etNumberInBase.setOnKeyListener(this);
     }
 
     @Override
@@ -37,9 +53,11 @@ public class SearchByNumberInBaseActivity extends AppCompatActivity implements V
         switch (view.getId()) {
             case R.id.search_by_number_in_base:
                 searchComparePensioner();
+                KeyboardUtils.hideKeyBoard(etNumberInBase, this);
                 break;
             case R.id.clear_button:
                 clearInputNumberInBase();
+                KeyboardUtils.initFocusAndShowKeyBoard(etNumberInBase, this);
                 break;
             case R.id.back_button:
                 onBackPressed();
@@ -94,5 +112,18 @@ public class SearchByNumberInBaseActivity extends AppCompatActivity implements V
     private void clearInputNumberInBase() {
         EditText numberTextView = findViewById(R.id.input_number_in_base);
         numberTextView.setText("");
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        // Обработайте нажатие клавиши, верните true, если
+        // обработка выполнена
+
+
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            searchComparePensioner();
+            KeyboardUtils.hideKeyBoard(etNumberInBase, this);
+        }
+        return false;
     }
 }
